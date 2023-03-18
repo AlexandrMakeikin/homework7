460b61165fff75bdf862c7f43c621b249e6f7282
lacky fish <makeykin87@mail.ru>
2023-03-18 17:56:07 +0300

    homework.seminar 7

---
+++ task 47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+double[,] GenerateMatrix()
+{
+    Console.WriteLine("введите количество строк матрицы:  ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("введите количество столбцов матрицы:  ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    double[,] matrix = new double[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new Random().Next(10, 100)) / 10;
+        }
+    }
+    return matrix;
+
+}
+void PrintMatrix(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "   ");
+        }
+        System.Console.WriteLine();
+    }
+}
+var myMatrix = GenerateMatrix();
+PrintMatrix(myMatrix);
---
+++ task 50/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matr,int rows, int colums)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "  ");
+        }
+        System.Console.WriteLine();
+    }
+    
+}
+
+int rows = ReadInt("Введите индекс строки: ");
+int colums = ReadInt("Введите индекс столбца: ");
+int[,] numbers = new int[6, 6];
+FillArray(numbers);
+ PrintMatrix(numbers,rows,colums);
+ System.Console.WriteLine();
+if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) Console.WriteLine($"элемент массива = {numbers[rows, colums]}");
+else Console.WriteLine($"[{rows},{colums}] -> такого элемента в массиве нет");
